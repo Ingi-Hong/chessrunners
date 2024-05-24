b774371eeaec81a7581e82b7e98ad646f0037b05
Ingi-Hong <ingihong@bu.edu>
2024-05-24 15:19:15 -0400

    add keys n stuff

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
-);
+const VITE_SUPABASE_URL = "https://gyctzxbuklqutliozjov.supabase.co";
+const VITE_SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5Y3R6eGJ1a2xxdXRsaW96am92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2MzE0MjgsImV4cCI6MjAyODIwNzQyOH0.bt1rsbBrdL4-0s2DxHX9s_U9I0H0_DXhAq_cjnYMjrI";
+
+export const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_KEY);
